6283ff4a146e59da1456eba7c4ebd57c4e81c8e2
markusVJH <markus.heinila@pp.inet.fi>
2023-04-04 19:56:44 +0300

    icons_and_animations

--- src/Card.js
+++ src/Card.js
@@ -1,5 +1,8 @@
 import React from "react";
 import './Card.css';
+import {FaHeart, FaHeartBroken, FaThumbsDown, FaThumbsUp} from 'react-icons/fa';
+import { IoMdCloseCircle } from "react-icons/io";
+
 
 const Card = (props) => {
   const imgUrl = `https://source.unsplash.com/500x400/?${props.name}`;
@@ -10,11 +13,21 @@ const Card = (props) => {
       </div>
       <h1 className="name">{props.name}</h1>
       <div className="likes">
-        <button onClick={props.onDislike}>-</button>
-        <span className="likesCount">{props.likes}</span>
-        <button onClick={props.onLike}>+</button>
-      </div>
-      <button className="removeBtn" onClick={props.onRemove}>x</button>
+  <button onClick={props.onDislike}><FaThumbsDown size="25px" color="gray"/></button>
+  {props.likes >= 0 ? (
+    <span className="likesCount">
+      <FaHeart color= "red" size= "25px"/>
+      <span className="likesValue">{props.likes}</span>
+    </span>
+  ) : (
+    <span className="likesCount">
+      <FaHeartBroken color= "gray" size= "25px"/>
+      <span className="likesValue">{props.likes}</span>
+    </span>
+  )}
+  <button onClick={props.onLike}><FaThumbsUp color="green" size= "25px"/></button>
+</div>
+      <button className="removeBtn" onClick={props.onRemove}><IoMdCloseCircle size= "2.5rem"/></button>
     </div>
   );
 };
